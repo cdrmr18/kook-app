fd0d7401870d6b4648d457ac2781cd37acbe922b
Kathy Tavia <kathy.tavia@gmail.com>
2021-02-19 00:13:26 +0100

    Completing and testing seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,20 +1,20 @@
 
 puts 'Deleting current seed...'
 
-User.delete_all
-Chef.delete_all
-Recipe.delete_all
-Booking.delete_all
-Chef_review.delete_all
-Recipe_review.delete_all
-Chat.delete_all
-Message.delete_all
+# User.delete_all
+# Chef.delete_all
+# Recipe.delete_all
+# Booking.delete_all
+# Chef_review.delete_all
+# Recipe_review.delete_all
+# Chat.delete_all
+# Message.delete_all
 
 puts 'Starting seed..'
 
 puts 'creating users...'
 10.times do
-  User.create(
+  User.create!(
     first_name: Faker::Name.first_name,
     last_name: Faker::Name.last_name,
     username: Faker::Internet.username,
@@ -25,140 +25,112 @@ end
 puts "#{User.count} users created"
 
 puts ' creating chefs...'
-Chef.create(
+Chef.create!(
   cuisine: 'Mexican',
   user_id: User.first.id,
   )
 
-Chef.create(
+Chef.create!(
   cuisine: 'Italian',
-  user_id: User.last,
-  )
-Chef.create(
-  cuisine: 'French',
-  user_id: User.first.id + 1,
+  user_id: User.last.id,
   )
 puts "#{Chef.count} chefs created"
 
 
 puts 'creating recipes...'
-10.times do
-  Recipe.create(
+5.times do
+  Recipe.create!(
     name: Faker::Food.dish,
     description: Faker::Food.description,
     ingredients: [[Faker::Food.measurement, Faker::Food.ingredient], [Faker::Food.measurement, Faker::Food.ingredient], [Faker::Food.measurement, Faker::Food.ingredient], [Faker::Food.measurement, Faker::Food.ingredient]],
-    cuisine: ['Mexican', 'Italian', 'French'].sample,
-    cook_time: rand(15..90),
-    chef_id: [Chef.first, Chef.first + 1, Chef.last].sample
+    cuisine: 'Mexican',
+    cook_time: rand(15..120),
+    chef_id: Chef.first.id
+    )
+end
+
+5.times do
+  Recipe.create!(
+    name: Faker::Food.dish,
+    description: Faker::Food.description,
+    ingredients: [[Faker::Food.measurement, Faker::Food.ingredient], [Faker::Food.measurement, Faker::Food.ingredient], [Faker::Food.measurement, Faker::Food.ingredient], [Faker::Food.measurement, Faker::Food.ingredient]],
+    cuisine: "Italian",
+    cook_time: rand(15..120),
+    chef_id: Chef.last.id
     )
 end
 puts "#{Recipe.count} recipes created"
 
 puts 'creating bookings...'
-Booking.create(
-  date: ,
-  start_time: Date.today + rand(1..14),
-  end_time: ,
-  video_url: ,
-  user_id: ,
-  recipe_id:
+Booking.create!(
+  date: Date.today,
+  start_time: Time.now,
+  end_time: Time.now,
+  video_url: 'www.video.com',
+  user_id: User.first.id,
+  recipe_id: Recipe.first.id
   )
-Booking.create(
-  date: Date.today + rand(1..14),
-  start_time: ,
-  end_time: ,
-  video_url: ,
-  user_id: ,
-  recipe_id:
-)
-Booking.create(
-  date: Date.today + rand(1..14),
-  start_time: ,
-  end_time: ,
-  video_url: ,
-  user_id: ,
-  recipe_id:
+Booking.create!(
+  date: Date.today,
+  start_time: Time.now,
+  end_time: Time.now,
+  video_url: 'www.video.com',
+  user_id: User.last.id,
+  recipe_id: Recipe.last.id
 )
 puts "#{Booking.count} bookings created"
 
-puts ' creating chefs...'
-Chef_review.create(
-  chef_id: ,
-  booking_id: ,
-  rating: ,
-  content:
+puts 'creating chefs...'
+ChefReview.create!(
+  chef_id: Chef.first.id,
+  booking_id: Booking.first.id,
+  rating: rand(1...5),
+  content: "It’s a great experience with this chef. They were very welcoming and charming. They are extremely knowledgeable and make great recommendations."
   )
-Chef_review.create(
-  chef_id: ,
-  booking_id: ,
-  rating: ,
-  content:
+ChefReview.create!(
+  chef_id: Chef.last.id,
+  booking_id: Booking.last.id,
+  rating: rand(1...5),
+  content: "What a kind a patient chef!"
   )
-Chef_review.create(
-  chef_id: ,
-  booking_id: ,
-  rating: ,
-  content:
-  )
-puts "#{Chef.count} chef reviews created"
+puts "#{ChefReview.count} chef reviews created"
 
 puts 'creating recipes...'
-Recipe_review.create(
-  chef_id: ,
-  name: ,
-  description: ,
-  ingredients: ,
-  cuisine: ,
-  cook_time:
+RecipeReview.create!(
+  rating: rand(1...5),
+  content: 'I have to say, I enjoyed every single bite of the meal',
+  recipe_id: Booking.last.recipe_id,
+  booking_id: Booking.last.id
   )
-Recipe_review.create(
-  chef_id: ,
-  name: ,
-  description: ,
-  ingredients: ,
-  cuisine: ,
-  cook_time:
+RecipeReview.create!(
+  rating: rand(1...5),
+  content: 'Excellent food.',
+  recipe_id: Booking.first.recipe_id,
+  booking_id: Booking.first.id
   )
-Recipe_review.create(
-  chef_id: ,
-  name: ,
-  description: ,
-  ingredients: ,
-  cuisine: ,
-  cook_time:
-  )
-puts "#{Recipe_review.count} recipe reviews created"
+puts "#{RecipeReview.count} recipe reviews created"
 
 puts ' creating chats...'
-Chat.create(
-  booking_id: ,
+Chat.create!(
+  booking_id: Booking.first.id,
   )
-Chat.create(
-  booking_id: ,
+Chat.create!(
+  booking_id: Booking.last.id,
   )
 puts "#{Chat.count} chats created"
 
 puts ' creating messages...'
-Message.create(
-  chat_id: ,
-  content: ,
-  user_id:
+Message.create!(
+  chat_id: Chat.first.id,
+  content: "Hi chef, I am excited to get cooking with you soon!",
+  user_id: Chat.first.booking_id
   )
-Message.create(
-  chat_id: ,
-  content: ,
-  user_id:
+Message.create!(
+  chat_id: Chat.last.id,
+  content: "It's nice to meet you chef. Can't wait to make your signature dish!",
+  user_id: Chat.booking_id
   )
-Message.create(
-  chat_id: ,
-  content: ,
-  user_id:
-  )
-Message.create(
-  chat_id: ,
-  content: ,
-  user_id:
-  )
+
 puts "#{Message.count} messages created"
 
 puts "Seeding complete"
