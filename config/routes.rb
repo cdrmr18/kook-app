8565c853e41760e0518ef6c4fa56f42544ca19e9
Kathy Tavia <kathy.tavia@gmail.com>
2021-02-18 13:44:00 +0100

    Added psuedo coded url paths to routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,20 +2,27 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
 
-
+  # /bookings -- /bookings/:id -- /bookings/:id/edit
   resources :bookings, only: [ :index, :show, :destroy, :edit, :update] do
+    # /bookings/:id/chef_reviews/new
     resources :chef_reviews, only: [:new, :create]
+    # /bookings/:id/recipe_reviews/new
     resources :recipe_reviews, only: [:new, :create]
   end
 
- # recipes/:id/bookings/new
+ # All CRUD actions for recipes
   resources :recipes do
+    # /recipes/:id/bookings/new
     resources :bookings, only: [:new, :create]
   end
 
-  resources :chefs, only: [:show, :new, :create, :index]
+  # /chats  -- /chats/:id
   resources :chats, only: [:index, :show] do
+    # chats/:id/messages
     resources :messages, only: [:create, :destroy]
   end
+
+  # /chefs/:id -- /chefs/new  -- /chefs
+  resources :chefs, only: [:show, :new, :create, :index]
 
 end
