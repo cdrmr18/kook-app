ebe4c063bc64b1a834880fd1700eafaf62183330
Kathy Tavia <kathy.tavia@gmail.com>
2021-02-19 14:25:57 +0100

    Added model validations and relationships

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,6 +1,12 @@
 class Recipe < ApplicationRecord
   belongs_to :chef
   has_many :bookings
-  has_many :recipe_reviews
+  has_many :recipe_reviews, through :bookings
   has_many_attached :photos
+
+  validates :name, presence: true
+  validates :description, presence: true
+  validates :ingredients, presence: true
+  validates :cuisine, presence: true
+  validates :cook_time, presence: true
 end
