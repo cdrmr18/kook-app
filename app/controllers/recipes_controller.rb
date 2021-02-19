cbdf472deecf2b2271a7ac4ea4c6d67e214b6be6
Kathy Tavia <kathy.tavia@gmail.com>
2021-02-19 22:16:28 +0100

    Completed basic design for  recipe index page

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,8 +1,6 @@
 class RecipesController < ApplicationController
   def index
     @recipes = Recipe.all
-    # finding user to use attached photo for chef photo
-    @user = User.find(Chef.find(Recipe.first.chef.user_id).user_id).photo
   end
 
   def create
