072c46c3370f8fe01bc71c9f6210bf14ecbc202a
Kathy Tavia <kathy.tavia@gmail.com>
2021-02-19 01:03:20 +0100

    Starting model validation implementation

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -2,6 +2,6 @@ class Booking < ApplicationRecord
   belongs_to :user
   belongs_to :recipe
   has_one :chat
-  has_many :chef_reviews
-  has_many :recipe_reviews
+  has_many :chef_reviews, dependent: :destroy
+  has_many :recipe_reviews, dependent: :destroy
 end
