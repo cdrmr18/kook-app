0726f21a5db233d11517e6b0f0c7b6194d29ddf1
Kathy Tavia <kathy.tavia@gmail.com>
2021-02-19 17:38:37 +0100

    Added price to recipes table and updated recipe model validations for price

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -9,4 +9,5 @@ class Recipe < ApplicationRecord
   validates :ingredients, presence: true
   validates :cuisine, presence: true
   validates :cook_time, presence: true
+  validates :price, presence: true, format: { with: /\A\d+(?:\.\d{2})?\z/ }, numericality: { greater_than: 0, less_than: 1000000 }
 end
