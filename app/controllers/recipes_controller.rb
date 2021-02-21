59d23c4b0a1e2a4f259f0b881b55edf96be6a5ca
Kathy Tavia <kathy.tavia@gmail.com>
2021-02-21 11:37:12 +0100

    Added a recipe to show

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -13,11 +13,17 @@ class RecipesController < ApplicationController
   end
 
   def show
+    @recipe = Recipe.find(params[:id])
   end
 
   def update
   end
 
   def destroy
+  end
+
+  private
+
+  def recipe_params
   end
 end
