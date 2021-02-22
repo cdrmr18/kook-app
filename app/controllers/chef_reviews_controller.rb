202ebbacceaf5cc2e8cc7f9acb473ab2e51f5260
Kathy Tavia <kathy.tavia@gmail.com>
2021-02-22 15:42:30 +0100

    Removed :new from review routes in order to display both review forms on booking show page

--- app/controllers/chef_reviews_controller.rb
+++ app/controllers/chef_reviews_controller.rb
@@ -1,5 +1,13 @@
 class ChefReviewsController < ApplicationController
   def create
+      @restaurant = Restaurant.find(params[:restaurant_id])
+      @review = Review.new(review_params)
+      @review.restaurant = @restaurant
+      if @review.save
+        redirect_to restaurant_path(@restaurant)
+      else
+        render 'orders/show'
+      end
   end
 
   def new
