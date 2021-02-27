0c0475a79afdc4b36c048606974541b12f103f5f
Rajeev Balendra <rj.balendra@gmail.com>
2021-02-27 12:45:42 +1100

    home page design with dragan

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,7 @@
 class PagesController < ApplicationController
   
   def home
-    @top_recipes = Recipe.joins(:recipe_reviews).where(recipe_reviews:{rating: 1})
+    @top_recipes = Recipe.joins(:recipe_reviews).where(recipe_reviews:{rating: 5})
   end
 
   
