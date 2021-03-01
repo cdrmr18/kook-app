43ab89c279d45c73d6f201f422f8b0bbcb5a4ef3
Kathy Tavia <kathy.tavia@gmail.com>
2021-03-01 14:17:11 +0100

    Setting authroize in recipe actions adn refactored recipe controller

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,4 +1,10 @@
 class RecipesController < ApplicationController
+  before_action :set_recipe, only: [:show, :update, :edit, :destroy]
+
+  def top
+    @recipes = Recipe.joins(:recipe_reviews).where(recipe_reviews:{rating: 1})
+  end
+
   def index
     if params[ :query ].present?
       @recipes = Recipe.search_by_cuisine_and_name(params[ :query ])
@@ -7,18 +13,25 @@ class RecipesController < ApplicationController
     end
   end
 
-
-  def top
-    @recipes = Recipe.joins(:recipe_reviews).where(recipe_reviews:{rating: 1})
+  def show
+    # needed to add ingredient and measurements in recipe show page
+    @measurement = Measurement.new
+    # chef name to display link to chef
+    @chef_name = "#{@recipe.chef.user.first_name + " " + @recipe.chef.user.last_name}"
   end
 
+  def new
+    @recipe = Recipe.new
+    authorize @recipe
+  end
 
   def create
     @recipe = Recipe.new
     @recipe = Recipe.create(recipe_params)
+    # creating chef instance to link recipe chef to new recipe by current user
     chef = Chef.new(user_id: current_user.id, cuisine: @recipe.cuisine)
     @recipe.chef = chef
-    @recipe.save
+    authorize @recipe
 
     if @recipe.save
       redirect_to recipe_path(@recipe)
@@ -28,21 +41,7 @@ class RecipesController < ApplicationController
 
   end
 
-  def new
-    @recipe = Recipe.new
-    authorize @recipe
-  end
-
   def edit
-  end
-
-  def show
-    # recipes/:id
-    @recipe = Recipe.find(params[:id])
-
-    @measurement = Measurement.new
-
-    @chef_name = "#{@recipe.chef.user.first_name + " " + @recipe.chef.user.last_name}"
   end
 
   def update
@@ -51,12 +50,13 @@ class RecipesController < ApplicationController
   def destroy
   end
 
-
-
-
-
-
   private
+
+  def set_recipe
+     # recipes/:id
+    @recipe = Recipe.find(params[:id])
+    authorize @recipe
+  end
 
   def recipe_params
     params.require(:recipe).permit(:name, :description, :cuisine, :cook_time, :price, :photo)
--- app/policies/recipe_policy.rb
+++ app/policies/recipe_policy.rb
@@ -5,7 +5,23 @@ class RecipePolicy < ApplicationPolicy
     end
   end
 
-  def new?
+  def create?
+    true
+  end
+
+  def show?
+    true
+  end
+
+  def edit?
+    true
+  end
+
+  def update?
+    true
+  end
+
+  def destroy?
     true
   end
 end
