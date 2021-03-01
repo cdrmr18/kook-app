4d13f806b1a03572dc54a480519a76ded5db3c6f
Kathy Tavia <kathy.tavia@gmail.com>
2021-03-01 21:07:21 +0100

    Changing views of certain links

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -2,6 +2,7 @@ class DashboardsController < ApplicationController
   def chef_dashboard
     @chef = current_user.chef
     @bookings = @chef.bookings
+    @recipes = @chef.recipes
 
     skip_authorization
   end
@@ -9,5 +10,7 @@ class DashboardsController < ApplicationController
   def user_dashboard
     @user = current_user
     @bookings = @user.bookings
+
+    skip_authorization
   end
 end
