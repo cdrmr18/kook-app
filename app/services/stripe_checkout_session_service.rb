88f86ec5e3ed5bb4c46d60adc0a11e62ceb93087
Luis R <looes1@gmail.com>
2021-03-06 17:32:38 +1100

    deleted unneded method and added a few comments

--- app/services/stripe_checkout_session_service.rb
+++ app/services/stripe_checkout_session_service.rb
@@ -1,3 +1,4 @@
+# Required for Stripe / Webhooks
 class StripeCheckoutSessionService
   def call(event)
     booking = Booking.find_by(checkout_session_id: event.data.object.id)
--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -6,6 +6,6 @@ Rails.configuration.stripe = {
 
 Stripe.api_key = Rails.configuration.stripe[:secret_key]
 StripeEvent.signing_secret = Rails.configuration.stripe[:signing_secret]
-StripeEvent.configure do |events|
+StripeEvent.configure do |events|  #WEBHOOK STRIPE
   events.subscribe 'checkout.session.completed', StripeCheckoutSessionService.new
 end
