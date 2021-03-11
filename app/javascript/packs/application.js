da9a2052550f69696f58f3f432887260f073d9d1
Kathy Tavia <kathy.tavia@gmail.com>
2021-03-11 19:57:20 +0100

    started implementing star rating with jquery

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -15,6 +15,12 @@ require("channels")
 //
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
+import { initStarRating } from '../plugins/init_star_rating';
+
+initStarRating();
+
+
+
 require("flatpickr")
 // External imports
 import "bootstrap";
