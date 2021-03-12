779db6a7d62f46f755c933b96ad0ec307da12858
Kathy Tavia <kathy.tavia@gmail.com>
2021-03-12 10:21:13 +0100

    added permits for name and unsername

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,8 +8,8 @@ class ApplicationController < ActionController::Base
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:photo])
-    devise_parameter_sanitizer.permit(:account_update, keys: [:photo])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:photo, :first_name, :last_name, :username])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:photo, :first_name, :last_name, :username])
   end
 
   # Pundit: white-list approach.
