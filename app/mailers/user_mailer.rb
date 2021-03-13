56a96e7e62e54fee90323e9f7ac771d8eb1f8c1e
Luis R <looes1@gmail.com>
2021-03-13 23:30:56 +1100

    setup user and app mailer + local test

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,13 +1,9 @@
 class UserMailer < ApplicationMailer
+  default from: 'notifications@example.com'
 
-  # Subject can be set in your I18n file at config/locales/en.yml
-  # with the following lookup:
-  #
-  #   en.user_mailer.welcome.subject
-  #
-  def welcome
-    @greeting = "Hi"
-
-    mail to: "to@example.org"
+  def welcome_email
+    @user = params[:user]
+    @url  = 'http://example.com/login'
+    mail(to: @user.email, subject: 'Welcome to KooK')
   end
 end
--- test/mailers/previews/user_mailer_preview.rb
+++ test/mailers/previews/user_mailer_preview.rb
@@ -2,8 +2,8 @@
 class UserMailerPreview < ActionMailer::Preview
 
   # Preview this email at http://localhost:3000/rails/mailers/user_mailer/welcome
-  def welcome
-    UserMailer.welcome
+  def welcome_email
+    user = User.first
+    UserMailer.with(user: user).welcome_email
   end
-
 end
