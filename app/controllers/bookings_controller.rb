cd5d0fdd55a752fc1493a79a61aa52c47cca76dc
Kathy Tavia <kathy.tavia@gmail.com>
2021-03-16 09:45:31 +0100

    changed display of review if reviews have been completed

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -4,8 +4,8 @@ class BookingsController < ApplicationController
 
   def index
     @bookings = policy_scope(Booking)
-    @next_booking = @bookings[0]
-    @past_bookings = @bookings[1..-1]
+    @next_booking = @bookings.last
+    @past_bookings = @bookings[0..-1]
     # raise
 
     # byebug
